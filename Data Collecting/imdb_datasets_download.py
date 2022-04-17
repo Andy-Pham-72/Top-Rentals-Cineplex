fd0b7b405ee912096247b6275c159a89ac2b7cc2
Andy Pham <aqpham02@gmail.com>
2022-04-17 17:17:07 -0400

    updated for azure databricks and blob storage

--- Data Collecting/imdb_datasets_download.py
+++ Data Collecting/imdb_datasets_download.py
@@ -1,84 +1,90 @@
+# import custom module
+from customlib.config.configdirectory import config_directory
+from customlib.custom_logger.customlogger import set_logger
+from customlib.custom_selenium.init_chrome import init_chrome_browser
+from customlib.custom_selenium.lastest_download import latest_download_file
+
+# import some basic modules
 import os
 import time
+import csv
+from datetime import datetime
 
-# web scrapper modules
-from selenium import webdriver
+# import selenium module
+from selenium.webdriver.common.by import By
 
 # unzip modules
 import gzip
 import shutil
 
-# check for directories existence
-directory_0 = 'Data Collecting/imdb dataset/raw_data'
-directory_1 = 'Data Collecting/imdb dataset/extracted_data'
 
-if not os.path.isdir(directory_0):
-    os.makedirs(directory_0)
+# initilize config variable
+my_config = config_directory('imdb.ini','directory')
+blob_config = config_directory('blobstorage.ini','blob_storage')
 
-if not os.path.isdir(directory_1):
-    os.makedirs(directory_1)
+chrome_driver_path = my_config['chrome_path']
+url = my_config['url']
+download_dir = my_config['directory_0']
+log_file_name= my_config['log_file_name']
 
-def latest_download_file(path):
-    """
-    function to wait for pending downloads to finish 
-    """
-    os.chdir(path)
-    files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
-    newest = files[-1]
+# set logger
+logger, log_dir, log_name = set_logger(log_file_name)
 
-    return newest
-
-# download url
-url = 'https://datasets.imdbws.com'
-
-# path to the chrome driver
-chrome_path = 'Data Collecting/chromedriver'
-
-# custom path to the download directory
-download_dir = '/Volumes/Moon/SpringBoard/Top Rentals Cineplex/Data Collecting/imdb dataset/raw_data'
-chrome_options = webdriver.ChromeOptions()
-preferences = {"download.default_directory": download_dir ,
-               "directory_upgrade": True,
-               "safebrowsing.enabled": True }
-chrome_options.add_experimental_option("prefs", preferences)
-
-driver = webdriver.Chrome(chrome_path, chrome_options= chrome_options)
-driver.get(url)
+# initialize a Chrome browser
+driver = init_chrome_browser(download_dir, chrome_driver_path, url)
 
 # list of files
-download_list = ['title.akas.tsv.gz',
-                 'title.basics.tsv.gz',
-                 'title.crew.tsv.gz',
-                 'title.principals.tsv.gz',
-                 'title.ratings.tsv.gz',
-                 'name.basics.tsv.gz']
+download_list = ['title.akas.tsv.gz','title.basics.tsv.gz','title.crew.tsv.gz','title.principals.tsv.gz','title.ratings.tsv.gz','name.basics.tsv.gz']
 
 # download raw files                 
 for i in range(len(download_list)):
-    driver.find_element_by_link_text(f'{download_list[i]}').click()
-    print(f'downloading: {download_list[i]}.......')
+    try:
+        driver.find_element(by=By.LINK_TEXT,value=download_list[i]).click()
+        #driver.find_element(by=By.LINK_TEXT, value=download_list[i]).click()
+        print(f'downloading: {download_list[i]}.......')
+        logger.info("Found the download url #%s/%s."%(url,download_list[i]))
+        
+    except DeprecationWarning as err:
+        logger.warning("A message: {}".format(err))
+    except Exception as err:
+        logger.error("An error message: {}".format(err))
 
 # wait for all downloads to finish
-fileends = "crdownload"
-dir = r'/Volumes/Moon/SpringBoard/Top Rentals Cineplex/Data Collecting/imdb dataset/raw_data'
-while "crdownload" == fileends:
-    time.sleep(1)
-    newest_file = latest_download_file(path = dir)
-    if "crdownload" in newest_file:
-        fileends = "crdownload"
-    else:
-        fileends = "none"
+file_ends = my_config['file_ends']
 
-print('download complete!')
+while "crdownload" == file_ends:
+    time.sleep(1)
+    try:
+        newest_file = latest_download_file(path = download_dir)
+        if "crdownload" in newest_file:
+            file_ends = "crdownload"
+        else:
+            file_ends = "none"
+    except Exception as err:
+        print(err)
+
+logger.info('Downloaded all files')
+
+# set up an account access key 
+spark.conf.set(
+    "fs.azure.account.key.%s.blob.core.windows.net"%(storage_account),
+    storage_key)
+
+# move all the downloaded files to azure blob
+dbutils.fs.cp('file:%s'%(download_dir), 'wasbs://%s@%s.blob.core.windows.net//raw_data/'%(container, storage_account), True)
+logger.info('Moved all files to "raw_data" blob')
 
 # unzip gz files
 for i in range(len(download_list)):
     time.sleep(3)
-    directory_2 = '/Volumes/Moon/SpringBoard/Top Rentals Cineplex/Data Collecting/imdb dataset/raw_data/'
-    directory_3 = '/Volumes/Moon/SpringBoard/Top Rentals Cineplex/Data Collecting/imdb dataset/extracted_data/'
-    with gzip.open( directory_2 + {download_list[i]} , 'rb') as f_in:
-        with open( directory_3 + {download_list[i][:-3]} , 'wb') as f_out:
+    output_dir = my_config['directory_1']
+    with gzip.open( download_dir+"/"+ download_list[i] , 'rb') as f_in:
+        with open( output_dir +"/"+ download_list[i][:-3] , 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
 
-# close browser
-driver.quit()
+# move all the extracted files to azure blob
+dbutils.fs.cp('file:%s'%(output_dir), 'wasbs://%s@%s.blob.core.windows.net//extracted_data/'%(container, storage_account), True)
+logger.info('Moved all files to "extracted_data" blob')
+
+# transfer log file to azure blob storage
+dbutils.fs.cp('file:%s'%(blob_config['fs_log_dir']), 'wasbs://%s@%s.blob.core.windows.net//log/'%(container, storage_account), True)
