00676e95da9905cdf7325e4f81eccbbcb02e3fb2
Andy Pham <aqpham02@gmail.com>
2022-05-27 15:05:30 -0400

    updated SCD Type 2 & added synopsis table

--- Data Collecting/top_rentals_cineplex_scraper.py
+++ Data Collecting/top_rentals_cineplex_scraper.py
@@ -16,6 +16,13 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+# import pyspark modules
+from pyspark.sql.functions import row_number, monotonically_increasing_id, udf, lit, when, date_sub
+from pyspark.sql import Window
+
+# BlobClient
+from azure.storage.blob import BlobClient
+
 # import tmdbsimple module
 import tmdbsimple as tmdb
 
@@ -23,15 +30,17 @@ import tmdbsimple as tmdb
 import os
 import time
 import csv
-from datetime import datetime
+from datetime import datetime,timedelta
 
 # initilize config variable
 my_config = config_directory('cineplex.ini','directory')
-blob_config = config_directory('blobstorage.ini','blob_storage')
 tmdb_config = config_directory('tmdb.ini', 'key')
+blob_config = config_directory('blobstorage.ini','blob_storage')
 
+# assign config string into variables
 storage_name = blob_config["storage_name"]
 storage_key = blob_config["key"]
+conn_str = blob_config["conn_str"]
 container_name = blob_config["container_name"]
 log_file_name = "cineplex_scraper"
 inside_log_name = "cineplex_scraper"
@@ -43,13 +52,13 @@ logger, log_dir, log_name = set_logger(log_file_name)
 url = my_config['url']
 
 # path to the chrome driver
-chrome_path = my_config['chrome_path']
+chrome_driver_path = my_config['chrome_path']
 
 # in this case I don't need download path for the init_chrome_browser() method but I still put a directory anyway
 download_path = "/mnt/container-data/"
 
 # set chrome driver
-driver = init_chrome_browser(download_path, chrome_path, url)
+driver = init_chrome_browser(download_path, chrome_driver_path, url)
 
 # set timeout
 timeout = 5
@@ -81,13 +90,13 @@ for link in titles_links:
         xpath_title_1    = my_config['xpath_title_1']
         xpath_title_2    = my_config['xpath_title_2']
         xpath_year       = my_config['xpath_year']
-        xpath_synopsis_1 = my_config['xpath_synopsis_1']
-        xpath_synopsis_2 = my_config['xpath_synopsis_2']
+        #xpath_synopsis_1 = my_config['xpath_synopsis_1']
+        #xpath_synopsis_2 = my_config['xpath_synopsis_2']
 
         # retrieve data
         title = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, xpath_title_1))).text
         year  = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, xpath_year))).text
-        synopsis = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, xpath_synopsis_1))).text 
+        #synopsis = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, xpath_synopsis_1))).text 
        
         # slow down the execution
         time.sleep(5)
@@ -95,7 +104,7 @@ for link in titles_links:
     except TimeoutException as err:
         # retrieve data due to different html setup
         title = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, xpath_title_2))).text
-        synopsis = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, xpath_synopsis_2))).text 
+        #synopsis = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, xpath_synopsis_2))).text 
         logger.warning("An info message: {}".format(err))
         
     except Exception as err:
@@ -103,10 +112,13 @@ for link in titles_links:
         
     # append data to rentals list
     try:
-        rentals.append(RetrieveRental(title, year, synopsis))
+        rentals.append(RetrieveRental(title, year))
         
     except Exception as err:
         logger.error("An error message: {}".format(err))
+
+# quit driver
+driver.quit()
 
 # write csv file
 try: 
@@ -123,18 +135,27 @@ except Exception as err:
     logger.error("An error message: {}".format(err))
 
 # assign the csv to a variable
-table_1 = spark.read.option("header","true").option("sep",",").csv(directory_1[5:-4]+".csv")
+title_source = spark.read.option("header","true").option("sep",",").csv(directory_1[5:-4]+".csv")
 
 # assign api key of the imdb
 tmdb.API_KEY = tmdb_config['key']
 # set timeout
 tmdb.REQUESTS_TIMEOUT = (2,5)
 
-# find the imdb unique number from the top rental movie titles
-imdb_number=[]
+# create a list for the top rental movie titles and imdb_id
+imdb_number = []
+
 # initialize search engine
 search = tmdb.Search()
-for i in final_list:
+
+# convert to get the top rental title list
+final_list = title_source.select('title').toPandas()
+
+# ordering variable
+order_num = 0
+
+# for loop to add data into top rental titles
+for i in final_list['title']:
     # search by movie name
     response = search.movie(query = i)
     # assign the movie id
@@ -143,31 +164,191 @@ for i in final_list:
     movie = tmdb.Movies(id).info()
     print("Searching for corresponding imdb id...")
     print(movie['imdb_id'])
+    # ordering of the movies
+    order_num += 1
     # add the movie name and imdb id into variable
-    imdb_number.append([movie['imdb_id'], i])
+    if movie['imdb_id'] == None:
+        imdb_number.append([str(movie['id']), i, order_num]) # add the movie "database id" instead of "imdb_id" so we can for later update easily
+    else:
+        imdb_number.append([movie['imdb_id'], i, order_num])
 
-# create a dataframe from imdb_number variable
-column = ["imdb_id","title"]
-table_2 = spark.createDataFrame(data=imdb_number,schema =column)
 
-# join 2 dataframes
+# check if the top_synopsis.parquet file exists in the blob storage
+blob = BlobClient.from_connection_string(conn_str=conn_str, container_name="capstone", blob_name="top_synopsis/top_synopsis.parquet")
+
+# create a list for the top synopsis movie rentals
+top_synopsis = []
+
+if blob.exists() == True:
+    # assign top_synopsis.parquet into df
+    df = spark.read.parquet("wasbs://{}@{}.blob.core.windows.net/top_synopsis/top_synopsis.parquet".format(container_name, storage_name))
+    # for loop to add data into top_synopsis table
+    for i in final_list['title']:
+        # search by movie name
+        response = search.movie(query = i)
+        # assign the movie id
+        id = search.results[0]['id']
+        # assign info variable to look for "imdb_id"
+        movie = tmdb.Movies(id).info()
+        print("Searching for corresponding imdb id...")
+        print(movie['imdb_id'])
+        # assign the imdb_id from synopsis_table into a variable
+        list_id = synopsis_table.select('imdb_id').toPandas()['imdb_id']
+        if movie['imdb_id'] == None and movie['id'] not in list_id:
+            top_synopsis.append([str(movie['id']), movie['overview']])
+        elif movie['imdb_id'] not in list_id:
+            top_synopsis.append([movie['imdb_id'], movie['overview']])
+        else:
+            pass
+else:
+    # for loop to add data into top_synopsis table
+    for i in final_list['title']:
+        # search by movie name
+        response = search.movie(query = i)
+        # assign the movie id
+        id = search.results[0]['id']
+        # assign info variable to look for "imdb_id"
+        movie = tmdb.Movies(id).info()
+        print("Searching for corresponding imdb id...")
+        print(movie['imdb_id'])
+        if movie['imdb_id'] == None :
+            top_synopsis.append([str(movie['id']), movie['overview']])
+        else:
+            top_synopsis.append([movie['imdb_id'], movie['overview']])
+
+# create 2 dataframes from imdb_number variable
+# columns for the top synopsis
+column_1 = ["imdb_id", "synopsis"]
+synopsis_table = spark.createDataFrame(data=top_synopsis,schema =column_1)
+# adding surrogate key
+synopsis_table = synopsis_table.withColumn('synopsis_id', row_number().over(Window.orderBy(monotonically_increasing_id())))
 try:
-    table_3 = table_1.join(table_2, ['title'], "inner")
-    logger.info("Added 'imdb_id' column into dataframe")
+    table_source.write.parquet("wasbs://{}@{}.blob.core.windows.net/top_synopsis/top_synopsis.parquet".format(container_name, storage_name)) 
+    logger.info("Moved top_synopsis.parquet to azure 'top_synopsis' blob")
 except Exception as err:
     logger.warning("A warning message: {}".format(err))
-    
+
+# columns for the top rental 
+column_2 = ["src_imdb_id","src_title", "ordering"]
+table_source = spark.createDataFrame(data=imdb_number,schema =column_2)
+
 # set up an account access key 
 spark.conf.set(
-    "fs.azure.account.key.%s.blob.core.windows.net"%(storage_name),
-    storage_key)
+                "fs.azure.account.key.%s.blob.core.windows.net"%(storage_name),
+                storage_key )
 
-# transfer csv file into parquet and save to azure blob storage
-try:
-    table_3.write.parquet("wasbs://{}@{}.blob.core.windows.net/top_cineplex_rental/{}.parquet".format(container_name, storage_name,directory_1[-27:-4])) # eg: directory_1[-27:-4] is 'rentals-list-2022-04-21'
-    logger.info("Moved %s.parquet to azure 'top_cineplex_rental' blob"%(directory_1[-27:-4]))
-except Exception as err:
-    logger.warning("A warning message: {}".format(err))
-    
-# transfer log file to azure blob storage
-dbutils.fs.cp('file:%s'%(log_dir), 'wasbs://%s@%s.blob.core.windows.net//log/%s'%(container_name, storage_name, log_name))
+# variable for end_date later on
+high_date = datetime.strptime('9999-12-31', '%Y-%m-%d').date()
+print(high_date)
+# variable for current_date later on
+current_date = datetime.today().date()
+print(current_date)
+
+# assign historical day so we can find the corresponding parquet file
+days = timedelta(7)
+history = datetime.today().date() - days
+
+
+# check if the target file exists in the blob storage
+blob = BlobClient.from_connection_string(conn_str=conn_str, container_name="capstone", blob_name="top_cineplex_rental/rentals-list-{}.parquet".format(history))
+
+# assign dictionary for the column names in order
+column_names = ['imdb_id', 'title', 'ordering', 'is_current', 'start_date', 'end_date']
+
+if blob.exists() == True:
+
+    # prepare for merge - added effective and end date
+    table_source_new = table_source.withColumn('src_start_date', lit(current_date)).withColumn('src_end_date', lit(high_date))
+    # assign the target table in a variable
+    table_target = spark.read.parquet("wasbs://{}@{}.blob.core.windows.net/top_cineplex_rental/rentals-list-{}.parquet".format(container_name, storage_name, history))
+    # full merge, join on key column and also high data column to make only join to the lastest records
+    table_merge = table_target.join(table_source_new, (table_target.imdb_id == table_source_new.imdb_id) &
+                                                      (table_target.end_date == table_source_new.src_end_date), how = 'fullouter' )
+
+    #There are four possibilities for the actions:
+        # UPSERT: attributes have changed in the source and the existing records need to be expired and new records need to be inserted.
+        # NOTCURRENT: record is changed in source table and the records in target table need to be updated logically.
+        # INSERT: new records exist in source that need to be inserted into the target table directly.
+        # NOACTION: no changes to the attributes or the records in target table are not current.
+
+    # derive new column to indicate the action
+    table_merge = table_merge.withColumn( 'action',
+                                        when(table_merge.title != table_merge.src_title, 'UPSERT' )
+                                       .when(table_merge.src_imdb_id.isNull() & table_merge.is_current, 'NOTCURRENT')
+                                       .when(table_merge.imdb_id.inNull(), 'INSERT')
+                                       .otherwise('NOACTION')
+                                        )
+    table_merge.show()
+
+    # for records that need no action
+    table_merge_p1 = table_merge.filter(
+                                        table_merge.action == 'NOACTION').select(column_names)
+
+    # for records that need insert only
+    table_merge_p2 = table_merge.filter(
+                                        table_merge.action == 'INSERT').select(
+                                            table_merge.src_imdb_id.alias('imdb_id'),
+                                            table_merge.src_title.alias('title'),
+                                            table_merge.ordering,
+                                            lit(1).alias('is_current'),
+                                            table_merge.src_start_date.alias('start_date'),
+                                            table_merge.src_end_date.alias('end_date')
+                                             )
+    # for the records that are not current
+    table_merge_p3 = table_merge.filter( 
+                                        table_merge.action == 'NOTCURRENT').select(column_names).withColumn(
+                                            'is_current', lit(0))
+
+    # for the records that need to be set expired and then inserted
+    table_merge_p4_1 = table_merge.filter(
+                                        table_merge.action == 'UPSERT').select(
+                                            table_merge.src_imdb_id.alias('imdb_id'),
+                                            table_merge.src_title.alias('title'),
+                                            table_merge.ordering,
+                                            lit(1).alias('is_current'),
+                                            table_merge.src_start_date.alias('start_date'),
+                                            table_merge.src_end_date.alias('end_date')
+                                        )
+    table_merge_p4_2 = table_merge.filter(
+                                        table_merge.action == 'UPSERT').withColumn(
+                                            'end_date', date_sub(table_merge.src_start_date, 1)).withColumn(
+                                            'is_current', lit(0)).select(column_names)
+    # union all records together
+    table_merge_final = table_merge_p1.unionAll(table_merge_p2).unionAll(
+                            table_merge_p3).unionAll(table_merge_p4_1).unionAll(table_merge_p4_2)
+
+    table_merge_final.withColumn('rental_id', row_number().over(Window.orderBy(monotonically_increasing_id()))).orderBy('start_date').show() # adding surrogate key
+
+    # assign column names in order
+    column_names = ['rental_id','imdb_id', 'title', 'ordering', 'is_current', 'start_date', 'end_date']
+    table_merge_final.select(column_names)
+
+    logger.info("Merged all the tables into dataframe")
+    try:
+        table_merge_final.write.parquet("wasbs://{}@{}.blob.core.windows.net/top_cineplex_rental/{}.parquet".format(container_name, storage_name,directory_1[-27:-4])) # eg: directory_1[-27:-4] is 'rentals-list-2022-04-21'
+        logger.info("Moved %s.parquet to azure 'top_cineplex_rental' blob"%(directory_1[-27:-4]))
+    except Exception as err:
+        logger.warning("A warning message: {}".format(err))
+
+else:
+    # change the column names
+    table_source = table_source.select(
+                                       table_source.src_imdb_id.alias("imdb_id"),
+                                       table_source.src_title.alias("title"),
+                                       table_source.ordering)
+    # add the column names and values
+    table_source = table_source.withColumn('is_current', lit(1)).withColumn(
+                                            'start_date', lit(current_date)).withColumn(
+                                            'end_date', lit(high_date)).withColumn(
+                                            'rental_id', row_number().over(Window.orderBy(monotonically_increasing_id()))) # adding surrogate key
+    # assign column names in order
+    column_names = ['rental_id','imdb_id', 'title', 'ordering', 'is_current', 'start_date', 'end_date']
+    table_source.select(column_names)
+
+    logger.info("Added first data into dataframe")
+    # transfer csv file into parquet and save to azure blob storage
+    try:
+        table_source.write.parquet("wasbs://{}@{}.blob.core.windows.net/top_cineplex_rental/{}.parquet".format(container_name, storage_name,directory_1[-27:-4])) # eg: directory_1[-27:-4] is 'rentals-list-2022-04-21'
+        logger.info("Moved %s.parquet to azure 'top_cineplex_rental' blob"%(directory_1[-27:-4]))
+    except Exception as err:
+        logger.warning("A warning message: {}".format(err))
