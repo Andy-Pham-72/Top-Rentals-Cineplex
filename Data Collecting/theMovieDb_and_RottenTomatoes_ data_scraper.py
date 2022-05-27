59dd8d92eb5834c929e6415d9e4893831736e19f
Andy Pham <aqpham02@gmail.com>
2022-05-27 16:13:58 -0400

    updated code for "non" imdb_id title

--- Data Collecting/theMovieDb_and_RottenTomatoes_ data_scraper.py
+++ Data Collecting/theMovieDb_and_RottenTomatoes_ data_scraper.py
@@ -58,10 +58,12 @@ directory_1 = directory_1 + datetime.today().strftime('%Y-%m-%d')
 df = spark.read.option("header","true").parquet("wasbs://{}@{}.blob.core.windows.net/top_cineplex_rental/{}.parquet".format(container_name, storage_name,directory_1[-23:]))
 
 # query all the movie titles from the top rental movie dataframe
-title = df.select('title').toPandas()['title']
+titles = df.select('title').toPandas()['title']
 
 # create a list
 critics = []
+critics_null= 'null'
+
 
 # create variables to grab the elements of the url
 row_class = rotten_config['row_class']
@@ -72,66 +74,75 @@ top_critics_end_link = rotten_config['top_critics_end_link']
 search_url = rotten_config['search_url']
 
 # for loop to retrieve the dataset of top critics review from rotten tomatoes
-for i in title:
+for title in titles:
     # search movie info by the title
     try:
-        movie_scraper = MovieScraper(movie_title=i.lower())
         # assign imdb id for the corresponding movie title
-        imdb_id = a.select(a.imdb_id).filter(a.title == i).collect()[0][0]
-        # slower the script
-        time.sleep(1)
-        # logger
-        logger.info("Getting the imdb unique id: {}".format(imdb_id))
-        
-    except Exception as err:
-        logger.warning("A warning message: {}".format(err))
-        
-    try:
-        # get the rotten tomatoes movie link
-        movie_scraper.extract_metadata()
-        # combine the string for the top critics url
-        url = movie_scraper.url + top_critics_end_link
-        # logger
-        logger.info("Getting the rotten tomatoes top critic url: {}".format(url))
-        # initialize the chrome browser
-        driver = init_chrome_browser(download_path, chrome_path, url)
-        
-    except Exception as err:
-        logger.warning("A warning message: {}".format(err))
-        
-        # create a search link for the alternate url due to timeout issue from the code above
-        link = search_url+i.split()[0]
-        
-        for n in range(len(i.split()[1:])):
-            print(i.split()[n])
-            link= link + "%20" +i.split()[1:][n] # combine the components of the search link by adding "%20" eg: "https://www.rottentomatoes.com/search?search=the%20batman"
+        imdb_id = df.select(df.imdb_id).filter(df.title == title).collect()[0][0]
+        if imdb_id[:2] == 'tt':
+            movie_scraper = MovieScraper(movie_title=title.lower())
+            # slower the script
+            time.sleep(1)
             # logger
-        logger.info("Getting the search rotten tomatoes url for the alternate critic url: {}".format(link))
-        # initialize the chrome browser
-        driver_1 = init_chrome_browser(download_path, chrome_path, link)
-        # get the alternate url
-        url = WebDriverWait(driver_1, 10).until(EC.visibility_of_element_located((By.XPATH, alternate_class))).get_attribute("href")
-        # combine the string for the top critics url
-        url = url + top_critics_end_link
-        logger.info("Getting the alternate critic url: {}".format(url))
-        # initialize the chrome browser
-        driver = init_chrome_browser(download_path, chrome_path, url)
-    
-    # get all the critics review elements in the url
-    rows = driver.find_elements(by=By.XPATH, value= row_class)
-    
-    # retrieve all the top critic reviews from the url
-    for row in rows:
-        print("downloading review...")
-        critics_review = row.find_element(by=By.XPATH, value= review_class).text
-        critics_name = row.find_element(by=By.XPATH, value= name_class).text
-        # append the data into the list
-        critics.append((imdb_id,critics_name,critics_review))
-        
+            logger.info("Getting the imdb unique id: {}".format(imdb_id))
+            
+            try:
+                # get the rotten tomatoes movie link
+                movie_scraper.extract_metadata()
+                # combine the string for the top critics url
+                url = movie_scraper.url + top_critics_end_link
+                # logger
+                logger.info("Getting the rotten tomatoes top critic url: {}".format(url))
+                # initialize the chrome browser
+                driver = init_chrome_browser(download_path, chrome_path, url)
 
+            except Exception as err:
+                logger.warning("A warning message: {}".format(err))
+
+                # create a search link for the alternate url due to timeout issue from the code above
+                link = search_url+title.split()[0]
+
+                for n in range(len(title.split()[1:])):
+                    print(title.split()[n])
+                    link= link + "%20" + title.split()[1:][n] # combine the components of the search link by adding "%20" eg: "https://www.rottentomatoes.com/search?search=the%20batman"
+                    # logger
+                logger.info("Getting the search rotten tomatoes url for the alternate critic url: {}".format(link))
+                # initialize the chrome browser
+                driver_1 = init_chrome_browser(download_path, chrome_path, link)
+                # get the alternate url
+                url = WebDriverWait(driver_1, 10).until(EC.visibility_of_element_located((By.XPATH, alternate_class))).get_attribute("href")
+                # combine the string for the top critics url
+                url = url + top_critics_end_link
+                logger.info("Getting the alternate critic url: {}".format(url))
+                # initialize the chrome browser
+                driver = init_chrome_browser(download_path, chrome_path, url)
+
+            # get all the critics review elements in the url
+            rows = driver.find_elements(by=By.XPATH, value= row_class)
+
+            # retrieve all the top critic reviews from the url
+            for row in rows:
+                print("downloading review...")
+                critics_review = row.find_element(by=By.XPATH, value= review_class).text
+                critics_name = row.find_element(by=By.XPATH, value= name_class).text
+                # append the data into the list
+                critics.append((imdb_id,critics_name,critics_review))
+        
+        # for the title doesn't have the "imdb_id" yet
+        else: 
+            critics.append((imdb_id,critics_null,critics_null))
+            # logger
+            logger.info("Adding Null values for the title: {} that does not have 'imdb_id'".format(imdb_id))
+            
+    except Exception as err:
+        logger.warning("A warning message: {}".format(err))
+        
+# assign columns name
 columns = ['imdb_id', 'reviewer', 'review']
+# create data frame
 table = spark.createDataFrame(data=critics,schema =columns)
-table = table.withColumn("index", row_number().over(Window.orderBy(monotonically_increasing_id()))-1)
+# add review_id
+table = table.withColumn("review_id", row_number().over(Window.orderBy(monotonically_increasing_id())))
 logger.info("Finished retrieving all the top critic review from the top rental movie list")
 
 try:
